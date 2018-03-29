c486c71ebe14ade2f99d938f08f1924631d072e9
parsagh97 <parsaghaderi97@gmail.com>
2018-03-30 02:52:56 +0430

    a simple webserver with tornado and python client

---
+++ api.py
@@ -0,0 +1,130 @@
+import tornado.ioloop
+import tornado.web
+import torndb
+import sqlite3
+from sqlite3 import Error
+import tornado.auth
+
+class register(tornado.web.RequestHandler):
+    def get(self):
+        # self.render("templates/register.html", title="register")
+        action = self.get_argument('act')
+        name = self.get_argument('name')
+        passwd = self.get_argument('passwd')
+        conn = sqlite3.connect("user.db")
+        c = conn.cursor()
+        if action == 'reg':
+            c.execute('select * from USER where name =?' ,(name,))
+            i = 0
+            for row in c.fetchall():
+                i = i+1
+            if i>0:
+                self.write({'message':'false'})
+            else:
+                c.execute("insert into USER values (? ,? ,0)" ,(name ,passwd))
+                conn.commit()
+                self.write({'message':'true'})
+        elif action == 'log':
+            c.execute("select password from USER where name == ?" ,(name,)) 
+            result = c.fetchone()  
+            if str(passwd) == str(result[0]):
+                self.write({'message':'true'})                
+            else:
+                self.write({'message':'false'})
+        else:
+            self.write({'message':'wronge command'})
+    def post(self):
+        pass
+        
+class panel(tornado.web.RequestHandler):
+    def get(self):
+        self.render("templates/panel.html" ,title = "panel")
+    def post(self):
+        withd = self.get_argument('with_amount')
+        user = self.get_argument('name')
+        depo = self.get_argument('depo_amount')
+        trans = self.get_argument('trans_amount')
+        trans_to = self.get_argument('trans_to')
+        if withd != "":
+            self.redirect("http://localhost:8000/account?name={0}&act=with&amount={1}".format(user ,withd))
+        elif depo != "":
+            self.redirect("http://localhost:8000/account?name={0}&act=depo&amount={1}".format(user ,depo))
+        elif trans_to != "":
+            self.redirect("http://localhost:8000/account?name={0}&act=trans&amount={1}&rec={2}".format(user ,trans ,trans_to))
+        else:
+            self.wite("hello")
+
+class account(tornado.web.RequestHandler):
+    def get(self):
+        name = self.get_argument('name')#account owner
+        action = self.get_argument('act')#action withdrawl, deposit, transfer
+        conn = sqlite3.connect("user.db")
+        c = conn.cursor()
+        if action == 'with':
+            amount = self.get_argument('amount')#amount of money in action
+            c.execute("select asset from USER where name=?" ,(name,))
+            row = c.fetchall()
+            if int(row[0][0])-int(amount) <0:
+                response = {'message':'no_money'}
+                self.write(response)
+            else:
+                c.execute("update USER set asset = asset - ? where name = ?" ,(amount ,name))
+                conn.commit()
+                response = {'message':'true'}
+                self.write(response)
+        elif action == 'depo':
+            amount = self.get_argument('amount')#amount of money in action
+            c.execute("update USER set asset = asset + ? where name = ?" ,(amount ,name))
+            conn.commit()
+            response = {'message':'true'}
+            self.write(response)
+        elif action == 'trans':
+            amount = self.get_argument('amount')#amount of money in action
+            rec = self.get_argument('rec')#if the action is trans then this is the name of the reciver
+            c.execute("select asset from USER where name=?" ,(name,))
+            row = c.fetchall()
+            if int(row[0][0])-int(amount) <0:
+                response = {'message':'no_money'}
+                self.write(response)
+            else:
+                c.execute("select * from USER where name=?" ,(rec ,))
+                i = 0
+                for row in c.fetchall():
+                    i = i + 1
+                if i!=0:
+                    c.execute("update USER set asset = asset - ? where name = ?" ,(amount ,name))
+                    conn.commit()
+                    c.execute("update USER set asset = asset + ? where name = ?" ,(amount ,rec))
+                    conn.commit()
+                    response = {'message':'true'}
+                    self.write(response)
+                else:
+                    self.write({'message':'no reciever'})
+
+        elif action == 'balance':
+            c.execute("select asset from USER where name=?" ,(name,))
+            row = c.fetchall()
+            asset = row[0][0]
+            self.write({'message':str(asset)})
+        else:
+            response = {'message':'bad request'}
+            self.write(response)
+        c.execute("select * from USER where name = ?" ,(name ,))
+        for row in c.fetchall():
+            print(row)     
+   
+    def post(self):
+        pass
+        
+def main_app():
+    return tornado.web.Application([
+    (r"/reg", register,),
+    (r"/account", account),
+    (r"/panel" ,panel),
+    ])
+    
+if __name__ == "__main__":
+    
+    app = main_app()
+    app.listen(8000)
+    tornado.ioloop.IOLoop.current().start()
---
+++ test2.py
@@ -0,0 +1,71 @@
+import requests
+def register(name ,passwd):
+    response = requests.get('http://localhost:8000/reg?act=reg&name={}&passwd={}'.format(name ,passwd))
+    if response.json()['message'] == 'false':
+        print("this user already exists\n")
+    else:
+        print("you've been registerd\n")
+def login(name ,passwd):
+    #request to l
+    response = requests.get('http://localhost:8000/reg?act=log&name={}&passwd={}'.format(name ,passwd))
+    if response.json()['message'] == 'false':
+        print("wronge password\n")
+    else:
+        print("logged in\n")
+        while(1):
+            action = int(input("what do you want to do:\n1:balance\n2:deposit\n3:withdrawl\n4:transfer\n"))
+            if action==1:
+                balance(name)
+            elif action==2:
+                amount = input("how much do you want to deposit?\n")
+                depo(name ,amount)
+            elif action==3:
+                with_amount = input("how much do you want to withdrawl?\n")
+                withdrawl(name ,with_amount)
+            elif action==4:
+                rec = input("who do you wish to transfer to?\n")
+                trans_amount = input("how much do you want to transfer to?\n")
+                transfer(name ,trans_amount ,rec)
+            else:
+                return
+
+def withdrawl(user ,amount):
+    #withdrawl money
+    response = requests.get('http://localhost:8000/account?name={}&act=with&amount={}'.format(user ,amount))
+    if response.json()['message'] == 'true':
+        print("action done\n")
+        balance(user)
+    else:
+        print("action stoped\n")
+def depo(user ,amount):
+    #depo some money
+    response = requests.get('http://localhost:8000/account?name={}&act=depo&amount={}'.format(user ,amount))
+    if response.json()['message'] == 'true':
+        print("action done\n")
+        balance(user)
+    else:
+        print("action stoped\n")
+        
+def transfer(user , amount ,reciever):
+    #transfer moeny
+    response = requests.get('http://localhost:8000/account?name={}&act=trans&amount={}&rec={}'.format(user ,amount ,reciever))
+    if response.json()['message'] == 'false':
+        print("reciever is wrong or the amount is more than what you have\n")
+        balance(user)
+    else:
+        print("action is done\n")
+def balance(user):
+    respone = requests.get('http://localhost:8000/account?name={}&act=balance'.format(user))
+    print("your balance is "+respone.json()['message']+"\n")
+
+
+while(1):
+    action = int(input("enter your action \n1:register\n2:login\n"))
+    if action==1:
+        name = str(input("enter a name:\n"))
+        passwd = str(input("enter a password:\n"))
+        register(name ,passwd)
+    elif action == 2:
+        name = str(input("enter your name:\n"))
+        passwd = str(input("enter your password:\n"))
+        login(name ,passwd)
